8edad2b8365aa131d115f8aad2736d654251cf86
Fabpk90 <fabrizio1996@live.fr>
2020-09-26 19:10:14 +0200

    Adding beeeezier and some sprites

---
+++ Assets/Scripts/Utils/PathCreator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utils
+{
+    public class PathCreator : MonoBehaviour
+    {
+        [HideInInspector]
+        public Path path;
+
+        private void Start()
+        {
+            path = new Path(transform.position);
+        }
+
+        public void CreatePath()
+        {
+            
+        }
+    }
+}
