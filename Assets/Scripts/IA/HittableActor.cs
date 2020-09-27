284eca0d485245b024387a163480b916269b399f
Benoit TOTH <benoit.tothdenima@gmail.com>
2020-09-27 22:39:50 +0200

    Fix merge

--- Assets/Scripts/IA/HittableActor.cs
+++ Assets/Scripts/IA/HittableActor.cs
@@ -10,10 +10,12 @@ namespace IA
         public EventHandler OnDeath;
         public GameObject poopHat;
         public Animator umbrella;
+        public bool canBeHit = true;
         private static readonly int Hit = Animator.StringToHash("Hit");
 
         private void Start()
         {
+            canBeHit = true;
             if (Random.Range(0, 4) == 0)
             {
                 health = 2;
@@ -25,9 +27,6 @@ namespace IA
             }
         }
 
-
-        public bool canBeHit;
-        
         public virtual void TakeDamage(int amount)
         {
             if(!canBeHit) return;
