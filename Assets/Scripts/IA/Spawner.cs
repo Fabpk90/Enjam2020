a6dc6bdf56085436416136e91be727c804a9a890
Fabpk90 <fabrizio1996@live.fr>
2020-09-27 16:38:53 +0200

    Spawner working

--- Assets/Scripts/IA/Spawner.cs
+++ Assets/Scripts/IA/Spawner.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using PathCreation;
 using UnityEngine;
+using UnityTemplateProjects.IA;
 
 namespace IA
 {
@@ -12,9 +13,33 @@ namespace IA
         public float spawnCooldown;
         private CooldownTimer _timerToRespawn;
 
+        public int amountToSpawn;
+
         private void Start()
         {
+            _timerToRespawn = new CooldownTimer(spawnCooldown, true);
+            _timerToRespawn.TimerCompleteEvent += () =>
+            {
+                var go = Instantiate(prefabToSpawn, transform.position, Quaternion.identity); 
+                go.GetComponent<IAPathWalk>().path = path; 
+                amountToSpawn--;
+
+                if (amountToSpawn == 0)
+                    _timerToRespawn.Pause();
+            };
             
+            _timerToRespawn.Start();
+        }
+
+        public void NextWave(int amountToSpawn)
+        {
+            this.amountToSpawn = amountToSpawn;
+            _timerToRespawn.Start();
+        }
+
+        private void Update()
+        {
+            _timerToRespawn.Update(Time.deltaTime);
         }
     }
 }
